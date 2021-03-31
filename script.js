5757e2e1786efceac5f8eefd5ab46bd3160b58f2
Dvareh <73953096+Dvareh@users.noreply.github.com>
2021-03-31 18:53:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+var alpha = document.getElementById("input").value
+document.getElementById('input').value = null;
+function clr(){
+let x = document.getElementById('input').value
+document.getElementById('input').value = null;
+}
+function minus1()
+{
+var epsilon=document.getElementById("input").value;
+document.getElementById("input").value=epsilon.substring(0,epsilon.length-1)
+}
+function pl() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "+"
+}
+function c1o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "1"
+}
+function c2o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "2"
+}
+function c3o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "3"
+}
+function cro() {
+	var beta=document.getElementById("input").value
+	input.value=eval(beta)
+}
+function c4o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "4"
+}
+function c5o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "5"
+}
+function c6o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "6"
+}
+function c7o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "7"
+}
+function c8o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "8"
+}
+function c9o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "9"
+}
+function min() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "-"
+}
+function umn() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "*"
+}
+function dil() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "/"
+}
+function toc() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "9"
+}
+function crdo() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + ")"
+}
+function cldo() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "("
+}
+function c0o() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "0"
+}
+function toch() {
+	let a = document.getElementById("input").value
+	document.getElementById('input').value = input.value + "."
+}
